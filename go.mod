abad968fb0f8f450997140791156346114d5b5d3
Rochi Eko Pambudi <rochieko11@gmail.com>
2022-01-08 23:42:39 +0700

    update module v2

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/rochiekop/go-app-hello-world
 
 go 1.17
 
-require github.com/rochiekop/go-hello-world v1.0.0
+require github.com/rochiekop/go-hello-world/v2 v2.0.0
--- main.go
+++ main.go
@@ -3,12 +3,12 @@ package main
 import (
 	"fmt"
 
-	gohelloworld "github.com/rochiekop/go-hello-world"
+	gohelloworld "github.com/rochiekop/go-hello-world/v2"
 )
 
 func main() {
 
-	greets :=gohelloworld.HelloWorld()
+	greets :=gohelloworld.HelloWorld(" Rochi Eko Pambudi")
 	fmt.Println(greets)
 
 }
