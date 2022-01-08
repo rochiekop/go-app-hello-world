e49e6b1f96321a042e707489a3d26ef5fd100bb3
Rochi Eko Pambudi <rochieko11@gmail.com>
2022-01-08 23:02:23 +0700

    add go module

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/rochiekop/go-app-hello-world
+
+go 1.17
+
+require github.com/rochiekop/go-hello-world v1.0.0
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	gohelloworld "github.com/rochiekop/go-hello-world"
+)
+
+func main() {
+
+	greets :=gohelloworld.HelloWorld()
+	fmt.Println(greets)
+
+}
